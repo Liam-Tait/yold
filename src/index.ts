5092121f022e9c8a20b0ccff0e0186b3de5a9050
liam-tait <liamr60@gmail.com>
2024-07-23 03:40:15 +1200

    fix import location

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+export { getYearMonthDay } from "./getYearMonthDay.js";
 export { fromISODate, toISOString } from "./iso.js";
-export { fromYMD, getYearMonthDay } from "./yold.js";
+export { fromYMD } from "./yold.js";
 export { nextDay } from "./nextDay.js";
